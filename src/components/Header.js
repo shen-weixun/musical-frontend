46b63d9521bf367f030d33208c980017ca8a3927
shen-weixun <shenweixun11@gmail.com>
2025-06-05 13:14:36 +0800

    Fix ethers.utils usage in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
-import { Navbar, Container, Button, Nav, Modal, ListGroup } from 'react-bootstrap'
+import { Navbar, Container, Button, Nav, Modal, ListGroup } from 'react-bootstrap';
 import { React, useEffect, useState, Row } from "react";
 import { useNavigate } from "react-router-dom";
-import { ethers } from 'ethers';
+import { formatEther } from 'ethers'; // 單獨導入 formatEther
 import { FaShoppingCart } from 'react-icons/fa'; // 安裝 react-icons 並引入購物車圖標
 
-
-
-
-function Header({state, details}) {
+function Header({ state, details }) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -20,53 +17,49 @@ function Header({state, details}) {
   const EtherscanURL = "https://sepolia.etherscan.io/address/" + details.contractAddress;
 
   let navigate = useNavigate(); 
-  const routeChange = () =>{ 
+  const routeChange = () => { 
     let path = `/orders`; 
     navigate(path);
-    handleClose()
-  }
+    handleClose();
+  };
 
-  useEffect(() =>{
-    const contractBalance = async () =>{
-        const balance = await contract.getBalance();        
-        console.log(ethers.utils.formatEther(balance));
-        setContractBalance(ethers.utils.formatEther(balance));
-        
+  useEffect(() => {
+    const contractBalance = async () => {
+      const balance = await contract.getBalance();        
+      console.log(formatEther(balance)); // 使用 formatEther
+      setContractBalance(formatEther(balance)); // 使用 formatEther
     };
     contract && contractBalance();
-},[contract]);
+  }, [contract]);
 
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        setCurrency(response);
+        setLoading(false);
+      })
+      .catch(err => console.error(err));
+  }, []);
 
-useEffect(() => {
-        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
-        .then(response => response.json())
-        .then(response => {
-          console.log(response)
-          setCurrency(response);
-          setLoading(false);
-            
-        })
-        .catch(err => console.error(err));
-    },[])
-
-
-    return (
-        <>
-        <Navbar bg="dark" variant="dark">
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/"> Night Pour</Navbar.Brand>
-          <Nav className="me-auto" >
-            <Nav.Link href="/" style={{color:"#E9A178"}}>Home</Nav.Link>
-            <Nav.Link href="/about" style={{color:"#E9A178"}}>About</Nav.Link>
+          <Nav className="me-auto">
+            <Nav.Link href="/" style={{ color: "#E9A178" }}>Home</Nav.Link>
+            <Nav.Link href="/about" style={{ color: "#E9A178" }}>About</Nav.Link>
           </Nav>
           <Nav.Link href="/cart" style={{ color: "#E9A178" }}>
-              <FaShoppingCart style={{ marginRight: "5px" }} /> Cart
-            </Nav.Link>
+            <FaShoppingCart style={{ marginRight: "5px" }} /> Cart
+          </Nav.Link>
           <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Address: <a href="#" onClick={handleShow} style={{color:"#E9A178"}}>{details.accounts}</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
+            <Navbar.Text>
+              Address: <a href="#" onClick={handleShow} style={{ color: "#E9A178" }}>{details.accounts}</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
@@ -75,37 +68,35 @@ useEffect(() => {
           <Modal.Title>Account</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-        <ListGroup variant="flush">
-        <ListGroup.Item>Wallet Address: <b>{details.accounts}</b></ListGroup.Item>
-        <ListGroup.Item>Contract Address: <br /><a href={EtherscanURL} style={{color:"#000"}}><b>{details.contractAddress}</b> </a></ListGroup.Item>
-        <ListGroup.Item>Wallet Balance: <br/> <b>{details.balance} ETH</b></ListGroup.Item>
-        <ListGroup.Item>Contract Balance: <br/> <b>{contractBalance} ETH</b></ListGroup.Item>
-        <ListGroup.Item>ChainID: <br/><b>{details.chainId}</b></ListGroup.Item>
-         </ListGroup>
-         <Modal.Title style={{marginTop:"30px", textAlign:"center"}}>Exchange Rate</Modal.Title>
-         { loading ?       
-       <h3>No Data</h3>
-       :
-         <ListGroup variant="flush">
-        <ListGroup.Item>{currency[0].name} : <br/><b>{currency[0].current_price} USD</b></ListGroup.Item>
-        <ListGroup.Item>{currency[1].name} : <br/> <b>{currency[1].current_price} USD</b></ListGroup.Item>
-        <ListGroup.Item>{currency[2].name} : <br/><b>{currency[2].current_price} USD</b></ListGroup.Item>
-         </ListGroup>
-}
+          <ListGroup variant="flush">
+            <ListGroup.Item>Wallet Address: <b>{details.accounts}</b></ListGroup.Item>
+            <ListGroup.Item>Contract Address: <br /><a href={EtherscanURL} style={{ color: "#000" }}><b>{details.contractAddress}</b> </a></ListGroup.Item>
+            <ListGroup.Item>Wallet Balance: <br /> <b>{details.balance} ETH</b></ListGroup.Item>
+            <ListGroup.Item>Contract Balance: <br /> <b>{contractBalance} ETH</b></ListGroup.Item>
+            <ListGroup.Item>ChainID: <br /><b>{details.chainId}</b></ListGroup.Item>
+          </ListGroup>
+          <Modal.Title style={{ marginTop: "30px", textAlign: "center" }}>Exchange Rate</Modal.Title>
+          {loading ?       
+            <h3>No Data</h3>
+            :
+            <ListGroup variant="flush">
+              <ListGroup.Item>{currency[0].name} : <br /><b>{currency[0].current_price} USD</b></ListGroup.Item>
+              <ListGroup.Item>{currency[1].name} : <br /> <b>{currency[1].current_price} USD</b></ListGroup.Item>
+              <ListGroup.Item>{currency[2].name} : <br /><b>{currency[2].current_price} USD</b></ListGroup.Item>
+            </ListGroup>
+          }
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="light" style={{backgroundColor:"#E9A178"}} onClick={routeChange}>
+          <Button variant="light" style={{ backgroundColor: "#E9A178" }} onClick={routeChange}>
             Orders
           </Button>
         </Modal.Footer>
       </Modal>
-        
-        </>
-      );
+    </>
+  );
 }
 
 export default Header;
